bced64782cb440b0b8c94275b9d0868daace2958
grasbiel <bzsilva1998@gmail.com>
2024-05-21 21:50:56 -0300

    Inclusão de leitura de dados e exibição de mensagem

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Projeto Conta Banco!");
+        // TODO: Conhecer e importar a classe scanner
+        // TODO: Exibir as mensagens para o nosso usuário
+        // TODO: Obter pela classe scanner os valores digitados no terminal
+        // TODO: Exibir a mensagem final da conta criada
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agencia: ");
+        agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Digite o seu saldo: "); 
+        saldo = scanner.nextDouble();
+
+
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
